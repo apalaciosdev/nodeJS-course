f915a098c54a5b2ce960f80ae7a7d6c5d382b4e8
apalacios <apalacios.dev@gmail.com>
2022-01-17 12:55:05 +0100

    2101171255

--- helpers/multiply.js
+++ helpers/multiply.js
@@ -1,22 +1,24 @@
 const fs = require('fs')
 
-const createTable = async(table = 1) => {
+const createTable = async(table = 1, listTable=false) => {
 
   try {
     let salida = ''
   
-    console.clear()
-    console.log('------------')
-    console.log('Tabla del ' + table)
-    console.log('------------')
     
     for (let i = 1; i < 11; i++) {
       let result = table*i
-    
+      
       salida += `${table} x ${i} = ${result}\n`
     }
     
-    console.log(salida)
+    if(listTable){
+      console.clear()
+      console.log('------------')
+      console.log('Tabla del ' + table)
+      console.log('------------')
+      console.log(salida)
+    }
     
     fs.writeFileSync(`tabla-${table}.txt`, salida)
     console.log(`The file tabla-${table}.txt has been created!`);
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const { createTable } = require('./helpers/multiply')
+const argv = require('yargs')
+              .option('t', {
+                alias: 'table',
+                type: 'number',
+                demandOption: 'true' //required param
+              })
+              .option('l', {
+                alias: 'listTable',
+                type: 'boolean',
+                default: false
+              })
+              .check( (argv, options) => {
+                if(isNaN(argv.t)){
+                  throw 'Table should be a number'
+                }
+                return true
+              })
+              .argv
 
 
-const table = 3
+//console.log( argv )
 
-createTable(table)
+//console.log('table: yargs', argv.table) 
+
+//const table = 2
+
+
+
+createTable(argv.table, argv.listTable)
   .then( createdFile => console.log(createdFile, "created!") )
   .catch( err => console.log(err) )
