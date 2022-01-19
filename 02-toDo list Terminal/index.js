5f06bde87b375907ce727ee6af09d83c21b3c0e1
apalacios <apalacios.dev@gmail.com>
2022-01-19 11:27:25 +0100

    2101191127

--- 02-toDo list Terminal/index.js
+++ 02-toDo list Terminal/index.js
@@ -23,7 +23,7 @@ const main = async() => {
         break;
       
       case '2':
-        console.log(tasks._list)
+        console.log(tasks.arrList)
         break;
     
 
--- 02-toDo list Terminal/models/tasks.js
+++ 02-toDo list Terminal/models/tasks.js
@@ -11,6 +11,18 @@ class Tasks {
   
   _list = {}
 
+
+  get arrList(){
+    const list = []
+    Object.keys(this._list).forEach( key => {
+      const task = this._list[key]
+      list.push(task)
+    })
+
+    return list
+  }
+
+
   constructor(){
     this._list = {}
   }
