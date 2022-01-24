4c19089bddccca911d7500d5bc83981f8cccaaf9
apalacios <apalacios.dev@gmail.com>
2022-01-24 10:42:26 +0100

    2101241042

--- 03- Web Server/index.js
+++ 03- Web Server/index.js
@@ -11,6 +11,14 @@ app.get('/hello', (req, res) => {
   res.send('Hello world')
 })
 
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+})
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html')
 })
