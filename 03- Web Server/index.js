6b217a7e61aa610dc7d6d69cde4385b5ba214a67
apalacios <apalacios.dev@gmail.com>
2022-01-24 11:52:43 +0100

    2101241152

--- 03- Web Server/index.js
+++ 03- Web Server/index.js
@@ -1,22 +1,36 @@
 const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config()
+
 const app = express()
-const port = 8080
+const port = process.env.PORT
+
+
+
+//handlebars
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 
 //Static content
 app.use( express.static('./public') )
 
+const data = {
+  name: 'Aaron Palacios',
+  title: 'Handlebars Site'
+}
 
-
-app.get('/hello', (req, res) => {
-  res.send('Hello world')
+app.get('/', (req, res) => {
+  res.render('home', data)
 })
 
 app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
+  res.render('generic', data)
 })
 
 app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html')
+  res.render('elements', data)
 })
 
 app.get('*', (req, res) => {
